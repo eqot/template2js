3b2a611a3b5efc1975979b40ee69163c91889732
Ikuo Terado <eqobar@gmail.com>
2013-05-03 00:18:53 +0900

    Supported arguments for input and output files

--- template2js.js
+++ template2js.js
@@ -1,18 +1,30 @@
 
+var argv = require('argv');
 var _ = require('underscore');
 var fs = require('fs');
 
-var output = 'template.js';
+var args = argv.run();
+// console.log(args);
 
-var templates = {
-	foo: '<b><%= name %></b>',
-	bar: '<b><%= name %></b>'
-}
+var inputFile = args.targets[0];
+var inputNameBegin = input.lastIndexOf('/') + 1;
+var inputNameEnd = input.lastIndexOf('.');
+var inputName = input.slice(inputNameBegin, inputNameEnd);
+// console.log(inputNameBegin);
+// console.log(inputNameEnd);
+// console.log(inputName);
+
+var inputData = fs.readFileSync(inputFile, {encoding: 'utf8'});
+// console.log(inputData);
+
+var templates = {};
+templates[inputName] = inputData;
 
 var data = 'var template = {};\n';
-for (var name in templates) {
-	data += 'template[\'' + name + '\']=' + _.template(templates[name]).source.replace(/\n/g, '') + ';\n';
-}
-var fd = fs.openSync(output, 'w');
+_.each(templates, function(template, name) {
+	data += 'template[\'' + name + '\']=' + _.template(template).source.replace(/\n/g, '') + ';\n';
+});
 
-fs.writeSync(fd, data, 0);
+var outputFile = args.targets[1] || input.slice(0, inputNameEnd) + '.js';
+
+fs.writeFileSync(outputFile, data, {encoding: 'utf8'});
